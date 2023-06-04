9815dd596999221fa3422b47133474e206e86883
Vitor <vitormeneses87@gmail.com>
2023-06-04 17:14:12 -0300

    edge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,8 @@ export const metadata: Metadata = {
   description: '...'
 };
 
+export const runtime = 'edge';
+
 export default async function Home() {
   return (
     <div className=" flex flex-col gap-5 bg-conetnt">
