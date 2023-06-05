1c51deff7905017b87b0a4657a8a6e3969f7b05c
Vitor Meneses <vitormeneses87@gmail.com>
2023-06-04 23:08:52 -0300

    commit

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -10,37 +10,38 @@ const { Content, Sider } = LayoutAntd;
 function Layout({ children }: { children: React.ReactNode }) {
   const [collapsed, setCollapsed] = useState<boolean>(true);
   return (
-    <LayoutAntd>
-      <Sider
-        width={280}
-        className="sticky left-0 h-screen top-0 bottom-0 overflow-hidden duration-500"
-        trigger={null}
-        collapsed={collapsed}
-      >
-        <Sidebar
-          colapssed={collapsed}
-          buttonOpen={
-            <Button
-              type="text"
-              icon={
-                collapsed ? (
-                  <ChevronRight className="text-white w-6 h-auto" />
-                ) : (
-                  <ChevronLeft className="text-white w-6 h-auto" />
-                )
-              }
-              onClick={() => setCollapsed(!collapsed)}
-              className="hover:bg-mediaBlue/10 h-16 w-16 text-base"
-            />
-          }
-        />
-      </Sider>
-      <LayoutAntd>
-        <Content className=" bg-content text-black font-sans p-5 h-screen overflow-auto">
-          {children}
-        </Content>
-      </LayoutAntd>
-    </LayoutAntd>
+    // <LayoutAntd>
+    //   <Sider
+    //     width={280}
+    //     className="sticky left-0 h-screen top-0 bottom-0 overflow-hidden duration-500"
+    //     trigger={null}
+    //     collapsed={collapsed}
+    //   >
+    //     <Sidebar
+    //       colapssed={collapsed}
+    //       buttonOpen={
+    //         <Button
+    //           type="text"
+    //           icon={
+    //             collapsed ? (
+    //               <ChevronRight className="text-white w-6 h-auto" />
+    //             ) : (
+    //               <ChevronLeft className="text-white w-6 h-auto" />
+    //             )
+    //           }
+    //           onClick={() => setCollapsed(!collapsed)}
+    //           className="hover:bg-mediaBlue/10 h-16 w-16 text-base"
+    //         />
+    //       }
+    //     />
+    //   </Sider>
+    //   <LayoutAntd>
+    //     <Content className=" bg-content text-black font-sans p-5 h-screen overflow-auto">
+    //       {children}
+    //     </Content>
+    //   </LayoutAntd>
+    // </LayoutAntd>
+    <>{children}</>
   );
 }
 
