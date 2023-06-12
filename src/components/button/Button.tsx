8c0c5b237e33fb46293bd2bf58cbd985fda63016
Vitor Meneses De Jesus <vitormeneses87@gmail.com>
2023-06-11 21:59:29 -0300

    finally refatory

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,26 @@
+interface ButtonProps {
+  type?: 'button' | 'submit' | 'reset' | undefined;
+  ariaLabel?: string;
+  className?: string;
+  label: string;
+}
+
+export default function Button({
+  ariaLabel,
+  className,
+  type,
+  label
+}: ButtonProps) {
+  return (
+    <button
+      type={type}
+      className={
+        'bg-mainBlue text-black rounded transition-colors hover:bg-blue-200 p-1 min-w-[200px] w-min font-sans mb-2 ' +
+        className
+      }
+      aria-label={ariaLabel}
+    >
+      {label}
+    </button>
+  );
+}
