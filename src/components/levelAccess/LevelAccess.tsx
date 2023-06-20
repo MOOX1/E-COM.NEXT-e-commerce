7d1fa4121dbf6cd3d5bda8104585a0893d878aa2
Vitor Meneses De Jesus <vitormeneses87@gmail.com>
2023-06-20 01:32:46 -0300

    refactor of code of level access

--- src/components/levelAccess/LevelAccess.tsx
+++ src/components/levelAccess/LevelAccess.tsx
@@ -1,63 +1,62 @@
 import { Crown, Eye, Gem } from 'lucide-react';
 import Div from '../motion/Div';
+import { ReactNode } from 'react';
+
+interface LevelAccessProps {
+  icon: ReactNode;
+  title: string;
+  description: string;
+  subDescription: string;
+  seeMore?: string;
+}
+
+const LevelAccessItems: LevelAccessProps[] = [
+  {
+    icon: <Crown className="h-auto w-8 text-mediaBlue " />,
+    title: 'Admin Super',
+    description: 'O admin super tem direito de fazer qualquer mundaça no site',
+    subDescription: 'não recomendamos dar esse acesso para muitas pessoas'
+  },
+  {
+    icon: <Gem className="h-auto w-8 text-mediaBlue " />,
+    title: 'Admin Simple',
+    description:
+      'O admin Simple tem direito de editar e cadastrar produtos e tudo relacionado a eles',
+    subDescription: 'Recomendado para a maioria dos admins',
+    seeMore: 'Saiba mais...'
+  },
+  {
+    icon: <Eye className="h-auto w-8 text-mediaBlue " />,
+    title: 'Admin Viewer',
+    description:
+      'O admin Simple serve para observação de dados e emissão de relatórios',
+    subDescription: 'Recomendado para pessoas que manipulam apenas dados'
+  }
+];
 
 export default function LevelAccess() {
   return (
     <div className="flex h-full w-full p-3 text-base text-mainBlue ">
-      <Div
-        initial={{ x: -50, opacity: 0, scale: 0 }}
-        animate={{ x: 0, opacity: 1, scale: 1 }}
-        transition={{ delay: 0.3, ease: 'easeInOut', duration: 0.5 }}
-        className="flex w-2/6 flex-col items-center justify-evenly text-center"
-      >
-        <div className=" flex flex-col items-center justify-center">
-          <Crown className="h-auto w-8 text-mediaBlue " />
-          <p className=" text-mediaBlue">Admin Super</p>
-        </div>
-        <p className="w-3/4  text-sm ">
-          O admin super tem direito de fazer qualquer mundaça no site
-        </p>
-        <p className=" w-3/4 text-xs  text-white/60">
-          não recomendamos dar esse acesso para muitas pessoas
-        </p>
-      </Div>
-      <Div
-        initial={{ x: -50, opacity: 0, scale: 0 }}
-        animate={{ x: 0, opacity: 1, scale: 1 }}
-        transition={{ delay: 0.4, ease: 'easeInOut', duration: 0.5 }}
-        className="flex w-2/6 flex-col items-center justify-evenly text-center"
-      >
-        <div className=" flex flex-col items-center justify-center">
-          <Gem className="h-auto w-8 text-mediaBlue " />
-          <p className=" text-mediaBlue">Admin Simple</p>
-        </div>
-        <div className="w-5/6 text-sm">
-          O admin Simple tem direito de editar e cadastrar produtos e tudo
-          relacionado a eles
-          <br />
-          <p className="underline">Saiba mais...</p>
-        </div>
-        <p className=" w-3/4 text-xs  text-white/60">
-          Recomendado para a maioria dos admins
-        </p>
-      </Div>
-      <Div
-        initial={{ x: -50, opacity: 0, scale: 0 }}
-        animate={{ x: 0, opacity: 1, scale: 1 }}
-        transition={{ delay: 0.5, ease: 'easeInOut', duration: 0.5 }}
-        className="flex w-2/6 flex-col items-center justify-evenly text-center"
-      >
-        <div className=" flex flex-col items-center justify-center">
-          <Eye className="h-auto w-8 text-mediaBlue " />
-          <p className=" text-mediaBlue">Admin Viewer</p>
-        </div>
-        <p className="w-3/4 text-sm ">
-          O admin Simple serve para observação de dados e emissão de relatórios
-        </p>
-        <p className=" w-3/4 text-xs  text-white/60">
-          Recomendado para pessoas que manipulam apenas dados
-        </p>
-      </Div>
+      {LevelAccessItems.map((item) => (
+        <Div
+          key={item.description}
+          initial={{ x: -50, opacity: 0, scale: 0 }}
+          animate={{ x: 0, opacity: 1, scale: 1 }}
+          transition={{ delay: 0.4, ease: 'easeInOut', duration: 0.5 }}
+          className="flex w-2/6 flex-col items-center justify-evenly text-center"
+        >
+          <div className=" flex flex-col items-center justify-center">
+            {item.icon}
+            <p className=" text-mediaBlue">{item.title}</p>
+          </div>
+          <div className="w-5/6 text-sm">
+            {item.description}
+            <br />
+            {item.seeMore && <p className="underline">{item.seeMore}</p>}
+          </div>
+          <p className=" w-3/4 text-xs  text-white/60">{item.subDescription}</p>
+        </Div>
+      ))}
     </div>
   );
 }
