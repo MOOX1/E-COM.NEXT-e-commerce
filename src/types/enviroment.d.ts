da82266ddb5e7c3f5b804834a74a183d07c4cc7e
Augusto TI <augustocesar4001@gmail.com>
2023-06-24 23:21:49 -0300

    chore: typing for environment variables

---
+++ src/types/enviroment.d.ts
@@ -0,0 +1,37 @@
+// Set the typing of your environment variables here 👇
+type MyVariables = {
+  // API
+  API_SECRET_KEY: string;
+
+  // EMAIL
+  EMAIL_SERVER_PORT: string;
+  EMAIL_FROM: string;
+  EMAIL_SERVER_USER: string;
+  EMAIL_SERVER_PASSWORD: string;
+  EMAIL_SERVER_HOST: string;
+
+  // UPSTASH REDIS
+  UPSTASH_REDIS_REST_URL: string;
+  UPSTASH_REDIS_REST_TOKEN: string;
+
+  // GOOGLE
+  GOOGLE_CLIENT_ID: string;
+  GOOGLE_CLIENT_SECRETE: string;
+
+  // NEXT AUTH
+  NEXTAUTH_SECRET: string;
+  NEXTAUTH_URL: string;
+  NEXT_PUBLIC_SECRET: string;
+
+  // MONGODB
+  MONGODB_URI: string;
+};
+
+declare global {
+  namespace NodeJS {
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    interface ProcessEnv extends MyVariables {}
+  }
+}
+
+export {};
