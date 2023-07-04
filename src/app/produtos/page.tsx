0989b9d1997d63cc47fe8f30926c179bf82db24d
Vitor Meneses De Jesus <vitormeneses87@gmail.com>
2023-07-04 14:20:12 -0300

    details about the new screen of products

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -1,10 +1,43 @@
-import Link from 'next/link';
-
-export default function Teste() {
+export default function Produtos() {
   return (
-    <Link href={'/'} className="font-sans">
-      {' '}
-      home
-    </Link>
+    <div className="flex flex-col gap-4">
+      <div className="flex h-full w-full gap-4">
+        <div className="flex min-h-[100px] w-full flex-1 items-center justify-center bg-strongBlue">
+          Lista de usuários
+        </div>
+        <div className="flex min-h-[100px] w-full flex-1 flex-col items-center justify-center bg-strongBlue py-3 duration-300 hover:flex-[1.5]">
+          cadastrar produto
+          <h1 className="text-red-500"> Caracteristicas de produtos: </h1>
+          <ul className="list-decimal">
+            <li> Nome </li>
+            <li> Preço </li>
+            <li> Preço promocional </li>
+            <li> Descrição </li>
+            <li> Imagens </li>
+            <li> Caracteristicas de produto </li>
+            <li> Produtos similares </li>
+            <li> Quantidade de estoque </li>
+            <li> Categoria </li>
+            <li> SubCategorias </li>
+            <li> Descontos unitário </li>
+            <li> Fabricante </li>
+            <li> Codigo de indêntificação </li>
+            <li> Marca </li>
+            <li> Encomendar produto sem estoque </li>
+          </ul>
+        </div>
+        <div className="flex flex-1 items-center justify-center bg-strongBlue">
+          <h1> Editar produtos </h1>
+        </div>
+      </div>
+      <div className="flex gap-4">
+        <div className="flex min-h-[100px] flex-1 items-center justify-center bg-strongBlue">
+          {' '}
+          detalhes de produtos
+        </div>
+        <div className="min-h-[100px] flex-1 "></div>
+        <div className="min-h-[100px] flex-1 "></div>
+      </div>
+    </div>
   );
 }
