5c4e4f1b90f685ead7ca5b869ba4e7701dd49fad
meneses <vitormeneses87@gmail.com>
2023-10-26 21:11:50 -0300

    feat: news adjustments for next 14

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com']
-  }
+    remotePatterns: [
+      {
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
